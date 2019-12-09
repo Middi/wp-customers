857683c6cdb9711554415b0fd915d68e678bcbb0
Middi <richard@richardmiddleton.me>
2019-12-09 15:35:23 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Plugin Name: Customer User Role
+ * Plugin URI: https://richardmiddleton.me
+ * Description: Adds a customer user role to users.
+ * Version: 1.0
+ * Author: Richard Middleton
+ */
+
+add_role('customer', __( 'Customer' ), array(
+		'read'         => true,
+		'edit_posts'   => false,
+	)
+);
+
+add_action('wp_enqueue_scripts', 'enqueue_jquery_form');
+
+function enqueue_jquery_form() {
+	wp_enqueue_script('jquery-form');
+}
+
+add_action('wp_ajax_create_customer', 'rmb_create_customer');
+
+add_action('wp_ajax_nopriv_create_customer', 'rmb_create_customer');
+
+function rmb_create_customer() {
+	$user_id = username_exists( $_POST['user_name'] );
+	if ( !$user_id and email_exists($_POST['user_email']) == false ) {
+		$random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
+		$user_id = wp_create_user( $_POST['user_name'], $random_password, $_POST['user_email'] );
+		$user_id = wp_update_user( array( 'ID' => $user_id, 'role' => 'customer' ) );
+
+		wp_new_user_notification($user_id, $random_password);
+	}
+	else {
+		$random_password = __('User already exists.  Password inherited.');
+	}
+}
+
+add_action('wp_ajax_update_customer', 'rmb_update_customer');
+
+function rmb_update_customer() {
+	update_user_meta( get_current_user_id(), 'user_address', $_POST['user_address'] );
+}
+add_action( 'admin_menu', 'register_my_custom_menu_page' );
+function register_my_custom_menu_page() {
+  // add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+  add_menu_page( 'Customers', 'Customers', 'manage_options', 'customer.php', 'customer_page', 'dashicons-welcome-widgets-menus', 90 );
+
+  function customer_page(){
+	?>
+<div class="wrap">
+    <h2>Welcome To My Plugin</h2>
+    <?php
+		$blogusers = get_users( 'role=customer' );
+		// Array of WP_User objects.
+		foreach ( $blogusers as $user ) {
+			echo '<span>' . esc_html( $user->user_email ) . '</span>';
+		}?>
+</div>
+<?php
+}
+}
+
